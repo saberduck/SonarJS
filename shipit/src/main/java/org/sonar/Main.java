abeeaba612f21127f1460f2f4985ef7419109660
Tibor Blenessy <tibor.blenessy@sonarsource.com>
2018-05-17 15:56:21 +0200

    Add metadata

--- shipit/src/main/java/org/sonar/Main.java
+++ shipit/src/main/java/org/sonar/Main.java
@@ -19,51 +19,199 @@ package org.sonar;/*
  */
 
 import com.google.gson.Gson;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.sonar.api.batch.fs.internal.DefaultInputFile;
 import org.sonar.api.batch.fs.internal.TestInputFileBuilder;
-import org.sonar.javascript.checks.AlertUseCheck;
+import org.sonar.check.Rule;
+import org.sonar.javascript.checks.CheckList;
 import org.sonar.javascript.checks.verifier.TestUtils;
 import org.sonar.javascript.visitors.JavaScriptVisitorContext;
 import org.sonar.plugins.javascript.api.JavaScriptCheck;
-import org.sonar.plugins.javascript.api.visitors.Issue;
+import org.sonar.plugins.javascript.api.visitors.FileIssue;
+import org.sonar.plugins.javascript.api.visitors.LineIssue;
 import org.sonar.plugins.javascript.api.visitors.PreciseIssue;
 
 public class Main {
 
+  private static final String PROFILE_JSON = "org/sonar/l10n/javascript/rules/javascript/Sonar_way_profile.json";
   static Gson gson = new Gson();
 
-  public static void main(String[] args) throws Exception {
-    System.out.println("Running analysis");
+  static Map<JavaScriptCheck, Metadata> metadata = new HashMap<>();
 
-    String input = readInput();
+  public static void main(String[] args) throws Exception {
+    if (args.length == 0) {
+      throw new IllegalArgumentException("Missing js file as argument");
+    }
+    List<JavaScriptCheck> checks = getChecks();
+    System.out.println("Found " + checks.size() + " checks");
+    String inputFilename = args[0];
+    long start = System.nanoTime();
+    System.out.println("Running analysis on " + inputFilename);
+    String input = readInput(inputFilename);
+    List<Issue> issues = analyze(checks, input);
+    saveIssues(issues, inputFilename);
+    long stop = System.nanoTime();
+    System.out.printf("Found %d issues in %d ms\n", issues.size(), (stop - start) / 1000_000);
+  }
+
+  private static String readInput(String filename) throws IOException {
+    return new String(Files.readAllBytes(Paths.get(filename)));
+  }
+
+  private static List<Issue> analyze(List<JavaScriptCheck> checks, String input) {
     DefaultInputFile inputFile = new TestInputFileBuilder("moduleKey", "file.js")
       .setContents(input)
       .setCharset(StandardCharsets.UTF_8)
       .build();
     JavaScriptVisitorContext context = TestUtils.createContext(inputFile);
-    List<Issue> actualIssues = getActualIssues(new AlertUseCheck(), context);
-    saveIssues(actualIssues);
+    return checks.stream()
+      .flatMap(check -> getActualIssues(check, context))
+      .collect(Collectors.toList());
   }
 
-  private static void saveIssues(List<Issue> actualIssues) throws IOException {
-    actualIssues.forEach(i -> {
-      if (i instanceof PreciseIssue) {
-        PreciseIssue preciseIssue = (PreciseIssue) i;
-        System.out.println(preciseIssue.primaryLocation().startLine());
-      }
-    });
+  private static void saveIssues(List<Issue> issues, String inputFilename) throws IOException {
+    if (issues.isEmpty()) {
+      return;
+    }
+    String uuid = UUID.randomUUID().toString();
+    new File("issues").mkdir();
+    BufferedWriter writer = Files.newBufferedWriter(Paths.get("issues","issues-" + uuid + ".json"));
+    issues.forEach(i -> i.path = inputFilename);
+    gson.toJson(issues, writer);
+    writer.close();
   }
 
-  private static String readInput() throws IOException {
-    return new String(Files.readAllBytes(Paths.get("input.js")));
+  static Stream<Issue> getActualIssues(JavaScriptCheck check, JavaScriptVisitorContext context) {
+    return check.scanFile(context).stream().map(Main::toIssue);
   }
 
-  public static List<Issue> getActualIssues(JavaScriptCheck check, JavaScriptVisitorContext context) {
-    return check.scanFile(context);
+  static List<JavaScriptCheck> getChecks() {
+    Set<String> sonarWayRuleKeys = sonarWayRuleKeys();
+    List<Class> allRules = CheckList.getChecks();
+    return allRules.stream()
+      .filter(rule -> isRuleInProfile(rule, sonarWayRuleKeys))
+      .map(Main::createCheck)
+      .collect(Collectors.toList());
   }
+
+  static JavaScriptCheck createCheck(Class rule) {
+    try {
+      JavaScriptCheck instance = (JavaScriptCheck) rule.newInstance();
+      Metadata metadata = new Metadata();
+      metadata.ruleKey = ruleKey(rule);
+      metadata.type = issueType(metadata.ruleKey);
+      Main.metadata.put(instance, metadata);
+      return instance;
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private static IssueType issueType(String ruleKey) {
+    String jsonFile = "org/sonar/l10n/javascript/rules/javascript/" + ruleKey + ".json";
+    InputStream resource = Main.class.getClassLoader().getResourceAsStream(jsonFile);
+    RuleJson ruleJson = gson.fromJson(new InputStreamReader(resource), RuleJson.class);
+    return ruleJson.type;
+  }
+
+  static boolean isRuleInProfile(Class rule, Set<String> sonarWayRuleKeys) {
+    return sonarWayRuleKeys.contains(ruleKey(rule));
+  }
+
+  private static String ruleKey(Class rule) {
+    Rule annotation = (Rule) rule.getAnnotation(Rule.class);
+    return annotation.key();
+  }
+
+  static Set<String> sonarWayRuleKeys() {
+    String jsonFile = PROFILE_JSON;
+    InputStream resource = Main.class.getClassLoader().getResourceAsStream(jsonFile);
+    if (resource == null) {
+      throw new IllegalStateException(jsonFile);
+    }
+    Profile profile = gson.fromJson(new InputStreamReader(resource), Profile.class);
+    return profile.ruleKeys;
+  }
+
+  private static Issue toIssue(org.sonar.plugins.javascript.api.visitors.Issue jsIssue) {
+    Metadata metadata = Main.metadata.get(jsIssue.check());
+    Issue issue = new Issue();
+    issue.ruleKey = metadata.ruleKey;
+    issue.type = metadata.type;
+    issue.message = message(jsIssue);
+    issue.line = line(jsIssue);
+    return issue;
+  }
+
+  private static String message(org.sonar.plugins.javascript.api.visitors.Issue jsIssue) {
+    if (jsIssue instanceof PreciseIssue) {
+      return ((PreciseIssue) jsIssue).primaryLocation().message();
+    }
+    if (jsIssue instanceof FileIssue) {
+      return ((FileIssue) jsIssue).message();
+    }
+    if (jsIssue instanceof LineIssue) {
+      return ((LineIssue) jsIssue).message();
+    }
+    return null;
+  }
+
+  private static Integer line(org.sonar.plugins.javascript.api.visitors.Issue jsIssue) {
+    if (jsIssue instanceof PreciseIssue) {
+      return ((PreciseIssue) jsIssue).primaryLocation().startLine();
+    }
+    if (jsIssue instanceof FileIssue) {
+      return 1;
+    }
+    if (jsIssue instanceof LineIssue) {
+      return ((LineIssue) jsIssue).line();
+    }
+    return null;
+  }
+
+  static class Profile {
+    String name;
+    Set<String> ruleKeys;
+  }
+
+  enum IssueType {
+    CODE_SMELL,
+    BUG,
+    VULNERABILITY
+  }
+
+  static class Metadata {
+    String ruleKey;
+    IssueType type;
+  }
+
+  static class RuleJson {
+    IssueType type;
+  }
+
+  static class Issue {
+    String ruleKey;
+    IssueType type;
+    String message;
+    Integer line;
+    String org;
+    String project;
+    String sha;
+    String path;
+  }
+
 }
