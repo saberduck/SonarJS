807746eac59bf4e2a31ffcea62cc642ded4bdb50
Massimo Paladin <massimo.paladin@gmail.com>
2018-05-17 19:12:08 +0200

    Read from BigQuery and write to text file

--- shipit/src/main/java/org/sonar/MainDataflow.java
+++ shipit/src/main/java/org/sonar/MainDataflow.java
@@ -19,87 +19,26 @@
  */
 package org.sonar;
 
+import com.google.api.services.bigquery.model.TableRow;
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.io.TextIO;
-import org.apache.beam.sdk.metrics.Counter;
-import org.apache.beam.sdk.metrics.Metrics;
+import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO;
 import org.apache.beam.sdk.options.Default;
 import org.apache.beam.sdk.options.Description;
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.options.Validation.Required;
-import org.apache.beam.sdk.transforms.Count;
 import org.apache.beam.sdk.transforms.DoFn;
-import org.apache.beam.sdk.transforms.MapElements;
-import org.apache.beam.sdk.transforms.PTransform;
 import org.apache.beam.sdk.transforms.ParDo;
-import org.apache.beam.sdk.transforms.SimpleFunction;
-import org.apache.beam.sdk.values.KV;
-import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.sdk.transforms.windowing.BoundedWindow;
 
 public class MainDataflow {
-
   /**
-   * Concept #2: You can make your pipeline assembly code less verbose by defining your DoFns
-   * statically out-of-line. This DoFn tokenizes lines of text into individual words; we pass it
-   * to a ParDo in the pipeline.
+   * GOOGLE_APPLICATION_CREDENTIALS=*** java -cp $PWD/SonarJS/shipit/target/shipit-bundled-4.1-SNAPSHOT.jar   org.sonar.MainDataflow   --project=project-test-199515   --stagingLocation=gs://project-test-199515/staging/   --output=gs://project-test-199515/output   --runner=DataflowRunner   --jobName=dataflow-intro13
    */
-  static class ExtractWordsFn extends DoFn<String, String> {
-    private final Counter emptyLines = Metrics.counter(ExtractWordsFn.class, "emptyLines");
-
-    @ProcessElement
-    public void processElement(ProcessContext c) {
-      if (c.element().trim().isEmpty()) {
-        emptyLines.inc();
-      }
-
-      // Split the line into words.
-      String[] words = c.element().split("[^\\p{L}]+");
-
-      // Output each word encountered into the output PCollection.
-      for (String word : words) {
-        if (!word.isEmpty()) {
-          c.output(word);
-        }
-      }
-    }
-  }
-
-  /** A SimpleFunction that converts a Word and Count into a printable string. */
-  public static class FormatAsTextFn extends SimpleFunction<KV<String, Long>, String> {
-    @Override
-    public String apply(KV<String, Long> input) {
-      return input.getKey() + ": " + input.getValue();
-    }
-  }
 
   /**
-   * A PTransform that converts a PCollection containing lines of text into a PCollection of
-   * formatted word counts.
-   *
-   * <p>Concept #3: This is a custom composite transform that bundles two transforms (ParDo and
-   * Count) as a reusable PTransform subclass. Using composite transforms allows for easy reuse,
-   * modular testing, and an improved monitoring experience.
-   */
-  public static class CountWords extends PTransform<PCollection<String>,
-    PCollection<KV<String, Long>>> {
-    @Override
-    public PCollection<KV<String, Long>> expand(PCollection<String> lines) {
-
-      // Convert lines of text into individual words.
-      PCollection<String> words = lines.apply(
-        ParDo.of(new ExtractWordsFn()));
-
-      // Count the number of times each word occurs.
-      PCollection<KV<String, Long>> wordCounts =
-        words.apply(Count.<String>perElement());
-
-      return wordCounts;
-    }
-  }
-
-  /**
-   * Options supported by {@link WordCount}.
+   * Options supported by .
    *
    * <p>Concept #4: Defining your own configuration options. Here, you can add your own arguments
    * to be processed by the command-line parser, and specify default values for them. You can then
@@ -116,6 +55,7 @@ public class MainDataflow {
     @Description("Path of the file to read from")
     @Default.String("gs://apache-beam-samples/shakespeare/kinglear.txt")
     String getInputFile();
+
     void setInputFile(String value);
 
     /**
@@ -124,6 +64,7 @@ public class MainDataflow {
     @Description("Path of the file to write to")
     @Required
     String getOutput();
+
     void setOutput(String value);
   }
 
@@ -132,11 +73,14 @@ public class MainDataflow {
       .as(WordCountOptions.class);
     Pipeline p = Pipeline.create(options);
 
-    // Concepts #2 and #3: Our pipeline applies the composite CountWords transform, and passes the
-    // static FormatAsTextFn() to the ParDo transform.
-    p.apply("ReadLines", TextIO.read().from(options.getInputFile()))
-      .apply(new CountWords())
-      .apply(MapElements.via(new FormatAsTextFn()))
+    p.apply(BigQueryIO.read().from("project-test-199515:github_us.massimoTest"))
+      .apply(ParDo.of(new DoFn<TableRow, String>() {
+                        @DoFn.ProcessElement
+                        public void processElement(ProcessContext c, BoundedWindow window) {
+                          c.output(c.element().get("repo_name").toString());
+                        }
+                      }
+      ))
       .apply("WriteCounts", TextIO.write().to(options.getOutput()));
 
     p.run().waitUntilFinish();
